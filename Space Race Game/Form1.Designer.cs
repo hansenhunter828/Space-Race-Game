c4b34b5eb8f39931431d0799f0d29a4732a5bb88
Hunter Hansen <hunthans828@ed.amdsb.ca>
2021-03-22 17:06:17 -0400

    Add project files.

---
+++ Space Race Game/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Space_Race_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subTitleLabel = new System.Windows.Forms.Label();
+            this.ship1ScoreLabel = new System.Windows.Forms.Label();
+            this.ship2ScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.titleLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(286, 92);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(153, 35);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Space Race";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // subTitleLabel
+            // 
+            this.subTitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.subTitleLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subTitleLabel.ForeColor = System.Drawing.Color.White;
+            this.subTitleLabel.Location = new System.Drawing.Point(185, 127);
+            this.subTitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.subTitleLabel.Name = "subTitleLabel";
+            this.subTitleLabel.Size = new System.Drawing.Size(351, 35);
+            this.subTitleLabel.TabIndex = 1;
+            this.subTitleLabel.Text = "Press Space To Start Or Escape To Exit";
+            this.subTitleLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ship1ScoreLabel
+            // 
+            this.ship1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ship1ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ship1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.ship1ScoreLabel.Location = new System.Drawing.Point(12, 468);
+            this.ship1ScoreLabel.Name = "ship1ScoreLabel";
+            this.ship1ScoreLabel.Size = new System.Drawing.Size(125, 29);
+            this.ship1ScoreLabel.TabIndex = 2;
+            this.ship1ScoreLabel.Text = "0";
+            // 
+            // ship2ScoreLabel
+            // 
+            this.ship2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ship2ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ship2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.ship2ScoreLabel.Location = new System.Drawing.Point(623, 468);
+            this.ship2ScoreLabel.Name = "ship2ScoreLabel";
+            this.ship2ScoreLabel.Size = new System.Drawing.Size(125, 29);
+            this.ship2ScoreLabel.TabIndex = 3;
+            this.ship2ScoreLabel.Text = "0";
+            this.ship2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(750, 500);
+            this.Controls.Add(this.ship2ScoreLabel);
+            this.Controls.Add(this.ship1ScoreLabel);
+            this.Controls.Add(this.subTitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subTitleLabel;
+        private System.Windows.Forms.Label ship1ScoreLabel;
+        private System.Windows.Forms.Label ship2ScoreLabel;
+    }
+}
+
---
+++ Space Race Game/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Space_Race_Game
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        #region Player Variables
+        int shipHeight = 20;
+        int shipWidth = 10;
+        int shipSpeed = 6;
+
+        int ship1Score = 0;
+        int ship2Score = 0;
+
+        int ship1Y = 480; int ship1X = 300;
+        int ship2Y = 480; int ship2X = 450;
+        int ship1XDefault = 300; int ship1YDefault = 480;
+        int ship2XDefault = 480; int ship2YDefault = 480;
+
+        #endregion
+        #region Player Movement
+        //Player 1
+        bool wDown = false;
+        bool sDown = false;
+
+        //Player 2
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        #endregion
+        #region Obstacle Variables
+        List<int> obstacle1XList = new List<int>();
+        List<int> obstacle1YList = new List<int>();
+        List<int> obstacle2XList = new List<int>();
+        List<int> obstacle2YList = new List<int>();
+
+        int obstacleHeight = 5;
+        int obstacleWidth = 20;
+        int obstacleSpeed = 6;
+        #endregion
+        #region Brushes
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        #endregion
+        string gameState = "waiting";
+
+        Random randGen = new Random();
+
+        int spawnCounter = 0;
+        int timer = 500;
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameInitialize()
+        {
+            titleLabel.Text = "";
+            subTitleLabel.Text = "";
+            ship1ScoreLabel.Text = "0";
+            ship2ScoreLabel.Text = "0";
+
+            gameTimer.Enabled = true;
+            gameState = "running";
+
+            ship1Reset();
+            ship2Reset();
+
+            obstacle1XList.Clear();
+            obstacle1YList.Clear();
+            obstacle2XList.Clear();
+            obstacle2YList.Clear();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                //Player 1
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                //Player 2
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Space:
+                    if(gameState == "waiting" || gameState == "gameOver")
+                    {
+                        GameInitialize();
+                    }
+                    break;
+                case Keys.Escape:
+                    if(gameState == "waiting" || gameState == "gameOver")
+                    {
+                        Application.Exit();
+                    }
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                //Player 1
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                //Player 2
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            #region Game Scenes
+            if (gameState == "waiting")
+            {
+                titleLabel.Text = "Space Race";
+                subTitleLabel.Text = "Press Space To Start Or Escape To Exit";
+            }
+            else if(gameState == "running")
+            {
+                titleLabel.Text = "";
+                subTitleLabel.Text = "";
+
+                #region Players
+                //Draw Players
+                e.Graphics.FillRectangle(whiteBrush, ship1X, ship1Y, shipWidth, shipHeight);
+                e.Graphics.FillRectangle(whiteBrush, ship2X, ship2Y, shipWidth, shipHeight);
+                #endregion
+                #region Obstacles
+                for (int i = 0; i < obstacle1XList.Count; i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, obstacle1XList[i], obstacle1YList[i], obstacleWidth, obstacleHeight);
+                }
+                for (int i = 0; i < obstacle2XList.Count; i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, obstacle2XList[i], obstacle2YList[i], obstacleWidth, obstacleHeight);
+                }
+                #endregion
+                for(int i = 0; i < timer; i++)
+                {
+                    e.Graphics.FillRectangle(whiteBrush, 373, 0, 10, 500 - timer) ;
+                }
+            }
+            else if(gameState == "gameOver")
+            {
+                if(ship1Score > ship2Score)
+                {
+                    titleLabel.Text = "Game Over";
+                    subTitleLabel.Text = $"Player 1 Won with {ship1Score} points";
+                }
+
+                if(ship2Score > ship1Score)
+                {
+                    titleLabel.Text = "Game Over";
+                    subTitleLabel.Text = $"Player 2 Won with {ship2Score} points";
+                }
+
+                if(ship1Score == ship2Score)
+                {
+                    titleLabel.Text = "Game Over";
+                    subTitleLabel.Text = $"Its a Tie!";
+                }
+            }
+            #endregion
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            timer--;
+            spawnCounter++;
+            #region Player Movement
+            //Player 1 Movement
+            if (wDown == true && ship1Y > 0)
+            {
+                ship1Y -= shipSpeed;
+            }
+            if (sDown == true && ship1Y < this.Height - shipHeight)
+            {
+                ship1Y += shipSpeed;
+            }
+            //Player 1 Movement
+            if (upArrowDown == true && ship2Y > 0)
+            {
+                ship2Y -= shipSpeed;
+            }
+            if (downArrowDown == true && ship2Y < this.Height - shipHeight)
+            {
+                ship2Y += shipSpeed;
+            }
+            #endregion
+            //Spawn Obstacles
+            if (spawnCounter == 10)
+            {
+                //Left Obstacles
+                obstacle1YList.Add(randGen.Next(10, this.Height - 50));
+                obstacle1XList.Add(10);
+                //Right Obstacles
+                obstacle2YList.Add(randGen.Next(10, this.Height - 50));
+                obstacle2XList.Add(750);
+
+                spawnCounter = 0;
+            }
+            #region Move Obstacles
+            for(int i = 0; i < obstacle1XList.Count; i++)
+            {
+                obstacle1XList[i] += obstacleSpeed;
+            }
+            for (int i = 0; i < obstacle2XList.Count; i++)
+            {
+                obstacle2XList[i] -= obstacleSpeed;
+            }
+            #endregion
+            #region Delete Obstacles
+            for(int i = 0; i < obstacle1XList.Count; i++)
+            {
+                if(obstacle1XList[i] > this.Width)
+                {
+                    obstacle1XList.RemoveAt(i);
+                    obstacle1YList.RemoveAt(i);
+                }
+            }
+            for (int i = 0; i < obstacle2XList.Count; i++)
+            {
+                if (obstacle2XList[i] < 0)
+                {
+                    obstacle2XList.RemoveAt(i);
+                    obstacle2YList.RemoveAt(i);
+                }
+            }
+            #endregion
+            #region Player Obstacle Collisions
+            Rectangle ship1Rec = new Rectangle(ship1X, ship1Y, shipWidth, shipHeight);
+            Rectangle ship2Rec = new Rectangle(ship2X, ship2Y, shipWidth, shipHeight);
+
+            for(int i = 0; i < obstacle1XList.Count; i++)
+            {
+                Rectangle obstacle1Rec = new Rectangle(obstacle1XList[i], obstacle1YList[i], obstacleWidth, obstacleHeight);
+                if (ship1Rec.IntersectsWith(obstacle1Rec))
+                {
+                    ship1Reset();
+                }
+                if (ship2Rec.IntersectsWith(obstacle1Rec))
+                {
+                    ship2Reset();
+                }
+            }
+            for (int i = 0; i < obstacle2XList.Count; i++)
+            {
+                Rectangle obstacle2Rec = new Rectangle(obstacle2XList[i], obstacle2YList[i], obstacleWidth, obstacleHeight);
+                if (ship1Rec.IntersectsWith(obstacle2Rec))
+                {
+                    ship1Reset();
+                }
+
+                if (ship2Rec.IntersectsWith(obstacle2Rec))
+                {
+                    ship2Reset();
+                }
+            }
+            #endregion
+            #region Scoring
+            if(ship1Y <= 0)
+            {
+                ship1Score++;
+                ship1ScoreLabel.Text = $"{ship1Score}";
+                ship1Reset();
+            }
+
+            if (ship2Y <= 0)
+            {
+                ship2Score++;
+                ship2ScoreLabel.Text = $"{ship2Score}";
+                ship2Reset();
+            }
+            #endregion
+            if(timer == 0)
+            {
+                gameState = "gameOver";
+                gameTimer.Enabled = false;
+            }
+
+            Refresh();
+        }
+
+        public void ship1Reset()
+        {
+            ship1X = ship1XDefault;
+            ship1Y = ship1YDefault;
+        }
+
+        public void ship2Reset()
+        {
+            ship2X = ship2XDefault;
+            ship2Y = ship2YDefault;
+        }
+    }
+}
